408c674ad56a31528de8081cb6c3a22f4c09f26d
Vijaye Raji <vijaye@statsig.com>
2023-03-18 15:46:58 +0700

    [Automated] Update Health Check Logs

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ function hideTooltip() {
 }
 
 async function genAllReports() {
-  const response = await fetch("urls.cfg");
+  const response = await fetch("urls.txt");
   const configText = await response.text();
   const configLines = configText.split("\n");
   for (let ii = 0; ii < configLines.length; ii++) {
