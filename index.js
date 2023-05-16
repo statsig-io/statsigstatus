2326d208b178828de83222f161c4fa4e9b637688
Daniel Loomb <daniel@statsig.com>
2023-05-16 09:42:26 -0700

    Remove all the extras from log_event

--- index.js
+++ index.js
@@ -182,9 +182,11 @@ function splitRowsByDate(rows) {
       continue;
     }
 
-    const [dateTimeStr, resultStr] = row.split(',', 2);
+    const [dateTimeStr, resultStr] = row.split(",", 2);
     // Replace '-' with '/' because Safari
-    const dateTime = new Date(Date.parse(dateTimeStr.replaceAll('-', '/') + ' GMT'));
+    const dateTime = new Date(
+      Date.parse(dateTimeStr.replaceAll("-", "/") + " GMT")
+    );
     const dateStr = dateTime.toDateString();
 
     let resultArray = dateValues[dateStr];
@@ -248,27 +250,40 @@ async function genAllReports() {
       continue;
     }
 
-    await genReportLog(document.getElementById("reports"), key, url);
+    const [cleanUrl] = url.split(" ");
+    if (!cleanUrl) {
+      continue;
+    }
+
+    await genReportLog(
+      document.getElementById("reports"),
+      key,
+      cleanUrl.replaceAll('"', "")
+    );
   }
 }
 
 async function genIncidentReport() {
-  const response = await fetch("https://incidents.statsig.workers.dev/contents");
+  const response = await fetch(
+    "https://incidents.statsig.workers.dev/contents"
+  );
   if (response.ok) {
     const json = await response.json();
     try {
-      const activeDom = DOMPurify.sanitize(marked.parse(json.active ? json.active : 'No active incidents'));
+      const activeDom = DOMPurify.sanitize(
+        marked.parse(json.active ? json.active : "No active incidents")
+      );
       const inactiveDom = DOMPurify.sanitize(marked.parse(json.inactive));
-      document.getElementById('activeIncidentReports').innerHTML = activeDom;
-      document.getElementById('pastIncidentReports').innerHTML = inactiveDom;
+      document.getElementById("activeIncidentReports").innerHTML = activeDom;
+      document.getElementById("pastIncidentReports").innerHTML = inactiveDom;
 
       if (json.active) {
         setTimeout(() => {
-          document.getElementById('incidents').scrollIntoView(true);
+          document.getElementById("incidents").scrollIntoView(true);
         }, 1000);
       }
     } catch (e) {
       console.log(e.message);
     }
-  } 
+  }
 }
